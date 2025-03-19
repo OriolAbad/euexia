f15223508cc02c62444f8422b25587163df65922
EnricMart <enric.moben@gmail.com>
2025-03-19 18:05:55 +0100

    backbutton

--- lib/app/modules/profile/views/profile_view.dart
+++ lib/app/modules/profile/views/profile_view.dart
@@ -5,10 +5,7 @@ import 'package:get/get.dart';
 import '../controllers/profile_controller.dart';
 import 'package:euexia/app/modules/account/views/account_view.dart';
 import 'package:euexia/app/modules/gallery/views/gallery_view.dart';
-
-//import 'stats_view.dart';
-//import 'gallery_view.dart';
-//import 'account_view.dart';
+import 'package:euexia/app/modules/home/views/home_view.dart'; // Importar HomeView
 
 class ProfileView extends StatelessWidget {
   @override
@@ -18,60 +15,61 @@ class ProfileView extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.black,
       body: Column( 
-          children: [
-            Stack(
-              children: [
-                Container(
-                  height: 400,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage('assets/gym_background.jpg'),
-                      fit: BoxFit.cover,
-                    ),
+        children: [
+          Stack(
+            children: [
+              Container(
+                height: 400,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/gym_background.jpg'),
+                    fit: BoxFit.cover,
                   ),
                 ),
-                Positioned(
-                  left: 16,
-                  top: 16,
-                  child: IconButton(
-                    icon: Icon(Icons.arrow_back, color: Colors.white),
-                    onPressed: () {},
-                  ),
-                ),
-                Positioned(
-                  bottom: 16,
-                  left: 16,
-                  child: Obx(() => Text(
-                    "Hi ${controller.username}!",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  )),
-                ),
-              ],
-            ),
-            SizedBox(height: 20),
-            _buildButton("Stats", () => Get.to(() => StatsView())),
-            _buildButton("Gallery", () => Get.to(() => GalleryView())),
-            _buildButton("Account", () => Get.to(() => AccountView())),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text("Log out"),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.grey,
               ),
+              Positioned(
+                left: 16,
+                top: 16,
+                child: IconButton(
+                  icon: Icon(Icons.arrow_back, color: Colors.white),
+                  onPressed: () {
+                    Get.off(() => HomeView()); // Navegar a HomeView
+                  },
+                ),
+              ),
+              Positioned(
+                bottom: 16,
+                left: 16,
+                child: Obx(() => Text(
+                  "Hi ${controller.username}!",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )),
+              ),
+            ],
+          ),
+          SizedBox(height: 20),
+          _buildButton("Stats", () => Get.to(() => StatsView())),
+          _buildButton("Gallery", () => Get.to(() => GalleryView())),
+          _buildButton("Account", () => Get.to(() => AccountView())),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text("Log out"),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.grey,
             ),
-          ],
-        ),
-      );
-
+          ),
+        ],
+      ),
+    );
   }
 
-  Widget _buildButton(String text,VoidCallback onPressed) {
+  Widget _buildButton(String text, VoidCallback onPressed) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 32),
       child: GestureDetector(
@@ -94,5 +92,3 @@ class ProfileView extends StatelessWidget {
     );
   }
 }
-
-
