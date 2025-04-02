bade12148828028d081991a8c00da887488c6929
OriolAbad <oriol.abad@sarria.salesians.cat>
2025-04-02 19:54:05 +0200

    punts del mapa venen de la base de dades

--- lib/app/modules/map/controllers/map_controller.dart
+++ lib/app/modules/map/controllers/map_controller.dart
@@ -3,42 +3,74 @@ import 'package:latlong2/latlong.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:flutter_map/flutter_map.dart'; // Importa flutter_map
 import 'package:flutter/material.dart';
-//import 'package:euexia/app/modules/map/service/map_service.dart'; // Importa el controlador
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class MapControllerX extends GetxController {
-  final MapController mapController = MapController(); // Controlador de flutter_map
-  final TextEditingController searchController = TextEditingController(); // Controlador del campo de búsqueda
-  Rx<LatLng?> searchLocation = Rx<LatLng?>(null); // Ubicación buscada (observable)
+  final MapController mapController =
+      MapController(); // Controlador de flutter_map
+  final TextEditingController searchController =
+      TextEditingController(); // Controlador del campo de búsqueda
+  Rx<LatLng?> searchLocation =
+      Rx<LatLng?>(null); // Ubicación buscada (observable)
   RxList<LatLng> markers = <LatLng>[].obs; // Lista de marcadores (observable)
 
   @override
   void onInit() {
     super.onInit();
-    
-    markers.add(LatLng(41.412422, 2.170818)); // Dir Castillejos
-    markers.add(LatLng(41.369252, 2.072708)); // Sant Joan Despí
+    fetchMarkers(); // Llama a la función para obtener los marcadores
+  }
+
+  void fetchMarkers() async {
+    final supabase = Supabase.instance.client;
+    try {
+      // Realiza la consulta a la tabla gimnasios
+      final response = await supabase
+          .from('gimnasios')
+          .select('latitud, longitud')
+          .execute();
+
+      if (response.data == null || response.data.isEmpty) {
+        print("No hay gimnasios en la base de datos");
+        return;
+      }
+
+      // Limpia los marcadores existentes
+      markers.clear();
+
+      // Añade los nuevos marcadores desde la base de datos
+      for (final gimnasio in response.data) {
+        addMarker(LatLng(
+            gimnasio['latitud'], gimnasio['longitud'])); // Agrega el marcador
+      }
+    } catch (e) {
+      print('Error al obtener los gimnasios: $e');
+    }
   }
 
   // Método para aumentar el zoom
   void zoomIn() {
     final double currentZoom = mapController.zoom;
-    mapController.move(mapController.center, currentZoom + 1); // Aumentar zoom en 1
+    mapController.move(
+        mapController.center, currentZoom + 1); // Aumentar zoom en 1
   }
 
   // Método para disminuir el zoom
   void zoomOut() {
     final double currentZoom = mapController.zoom;
-    mapController.move(mapController.center, currentZoom - 1); // Disminuir zoom en 1
+    mapController.move(
+        mapController.center, currentZoom - 1); // Disminuir zoom en 1
   }
 
   // Método para buscar una ubicación
   Future<void> searchLocationOnMap() async {
     try {
-      List<Location> locations = await locationFromAddress(searchController.text);
+      List<Location> locations =
+          await locationFromAddress(searchController.text);
       if (locations.isNotEmpty) {
-        searchLocation.value = LatLng(locations[0].latitude, locations[0].longitude);
-        mapController.move(searchLocation.value!, 12.0); // Mover el mapa a la ubicación buscada
-        markers.add(searchLocation.value!); // Agregar marcador
+        searchLocation.value =
+            LatLng(locations[0].latitude, locations[0].longitude);
+        mapController.move(searchLocation.value!,
+            12.0); // Mover el mapa a la ubicación buscada
       } else {
         Get.snackbar('Error', 'Ubicación no encontrada');
       }
